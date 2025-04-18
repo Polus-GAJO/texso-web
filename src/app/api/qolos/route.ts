2e58632a70a9e15298700360a518f98864e0d918
Polus- GAJO <poles_gajuo@yahoo.com>
2025-04-18 02:53:17 +0200

    Fix: handle unknown error by converting to string3

--- src/app/api/qolos/route.ts
+++ src/app/api/qolos/route.ts
@@ -1,12 +1,7 @@
 import connectToDatabase from '@/db';
 
 export async function GET() {
-  try {
-    const connection = await connectToDatabase();
-    const [rows] = await connection.execute('SELECT Qolo FROM Qolos');
-    return Response.json(rows);
-  } catch (error) {
-    return Response.json({ error: (error as Error).message });
-  }
-  }
+  const connection = await connectToDatabase();
+  const [rows] = await connection.execute('SELECT Qolo FROM Qolos');
+  return Response.json(rows);
 }
